9bd8e0dd1d2fb0b6e307219431c9a524d6645fe9
bambamboo15 <11bambamboo@gmail.com>
2025-03-22 22:15:41 -0500

    test

--- app.js
+++ app.js
@@ -11,8 +11,42 @@ app.get("/", function(req, res) {
 	res.sendFile("index.html", { root: __dirname });
 });
 
-app.get("/total-score-risk", function(req, res) {
-	// ouch!
+app.get("/score-risk", function(req, res) {
+	// get age, BMI, blood pressure, and family disease values
+	const { age, bmi, bp, fd } = req.query;
+
+	// calculation
+	let score = 0;
+
+	     if (age < 30) { score += 0;  }
+	else if (age < 45) { score += 10; }
+	else if (age < 60) { score += 20; }
+	else               { score += 30; }
+
+	     if (bmi === "normal")     { score += 0;  }
+	else if (bmi === "overweight") { score += 30; }
+	else                           { score += 75; }
+	
+	     if (bp === "normal")   { score += 0;   }
+	else if (bp === "elevated") { score += 15;  }
+	else if (bp === "stage-1")  { score += 30;  }
+	else if (bp === "stage-2")  { score += 75;  }
+	else if (bp === "crisis")   { score += 100; }
+
+	     if (fd === "diabetes")   { score += 10; }
+	else if (fd === "cancer")     { score += 10; }
+	else if (fd === "alzheimers") { score += 10; }
+
+	// calculate risk
+	let risk;
+
+	     if (score <= 20) { risk = "low risk";      }
+	else if (score <= 50) { risk = "moderate risk"; }
+	else if (score <= 75) { risk = "high risk";     }
+	else                  { risk = "uninsurable";   }
+
+	// send back total score and risk
+	res.json({ score, risk });
 });
 
 app.listen(port, function() {
